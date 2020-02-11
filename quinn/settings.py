29b5c0ffdc7c28dec9eee42611b92639617d0778
dextersbrain <denno.tz@gmail.com>
2020-02-11 21:52:18 +0300

    m

--- quinn/settings.py
+++ quinn/settings.py
@@ -127,10 +127,10 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"staticfiles","static_root")
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"staticfiles","media_root")
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-# import dj_database_url 
-# prod_db  =  dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
-# django_heroku.settings(locals())
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
+django_heroku.settings(locals())

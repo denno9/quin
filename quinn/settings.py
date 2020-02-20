d4d020875072c83094e6197f2d61c7f6b5d4ebb4
dextersbrain <denno.tz@gmail.com>
2020-02-20 12:40:37 +0300

    debug

--- quinn/settings.py
+++ quinn/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tjbvl-19d0h$e^n%&i2*u5hsoa=!_xg9gf-ru0u_s76vrz=aig'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.179','localhost''qcchambers.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','qcchambers.herokuapp.com','www.qcc.co.tz']
 
 
 # Application definition
